8d6ea21d1d6ccb9d43519924014307db1e8f9d30
Brijesh B S <brijeshbs007@gmail.com>
2020-05-23 20:44:16 +0530

    eyer_html

--- hiree/src/app/register-as-employer/register-as-employer.page.ts
+++ hiree/src/app/register-as-employer/register-as-employer.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-as-employer',
@@ -16,5 +17,24 @@ export class RegisterAsEmployerPage implements OnInit {
   ionViewWillEnter() {
     this.menuCtrl.enable(false);
   }
-  
+  onSubmit(form: NgForm) {
+    var postdata = {
+      user_id: 1,
+      eyer_gst: form.value.gst,
+      eyer_hotel_name: form.value.hotel_name,
+      eyer_address_1: form.value.address_1,
+      eyer_address_2: form.value.address_2,
+      eyer_city: form.value.city,
+      eyer_state: form.value.state,
+      eyer_website: form.value.website,
+      eyer_type: form.value.type,
+      eyer_category: form.value.category,
+      eyer_cuisines: form.value.Cuisines,
+      eyer_seat: form.value.seat,
+
+
+    }
+console.log(form.value.cuisines);
+
+  }
 }
