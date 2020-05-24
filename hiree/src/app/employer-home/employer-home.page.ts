fc0b111714fe5b97e641bf1db79c868fa26e921f
Ashish-3001 <cvnnashish@gmail.com>
2020-05-24 17:15:48 +0530

    modify

--- hiree/src/app/employer-home/employer-home.page.ts
+++ hiree/src/app/employer-home/employer-home.page.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { resolve } from 'url';
+import { promise } from 'protractor';
 
+
+@Injectable()
 @Component({
   selector: 'app-folder',
   templateUrl: './employer-home.page.html',
@@ -9,11 +14,31 @@ import { MenuController } from '@ionic/angular';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  
 
-  constructor(private activatedRoute: ActivatedRoute, public menuCtrl: MenuController) { }
+  results: object = [{ }];
+  loading: boolean = false;
+
+  constructor(private activatedRoute: ActivatedRoute, 
+    public menuCtrl: MenuController,
+    private http:HttpClient) 
+  { 
+    let promise= new Promise((resolve, reject ) =>{
+      this.http.get('http://127.0.0.1:8000/EmployeeDetails/').toPromise().then( (data) =>{
+      console.log(data);
+      this.results = data;
+      resolve();
+    },
+    msg => {
+      reject();
+    }
+    );
+    });
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
   }
 
   ionViewWillEnter() {
