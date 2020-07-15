2471a7e66956f958ce464201c352ac4ca55ad7b7
Ashish-3001 <cvnnashish@gmail.com>
2020-07-15 15:34:00 +0530

    trobuleshooting

--- hiree/src/app/register/register.page.ts
+++ hiree/src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public data:object = [{ }];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://hiree-back-end.herokuapp.com/UserLogin/').subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
   }
 
 }
